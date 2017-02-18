22b8143d5da2967d49192548b3bcbc38d2e81e5e
Robert Vunabandi <rvunabandi@gmail.com>
2017-02-18 11:26:27 -0500

    added frontend folder

---
+++ frontend/main.js
@@ -0,0 +1,83 @@
+const regions = {
+	north: {}
+	east: {}
+	south: {}
+	west: {}
+	central: {}
+}
+
+const africanCountries = ["Algeria","Angola","Benin","Botswana","Burkina Faso","Burundi","Cabo Verde","Cameroon","Central African Republic (CAR)","Chad","Comoros","Democratic Republic of the Congo","Republic of the Congo","Cote d'Ivoirez","Djibouti","E","Egypt","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea-Bissau","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"];
+
+countries2 = {};
+for (x in africanCountries){
+	countries2[x] = africanCountries[x];
+	// console.log(x, africanCountries[x]);
+}
+console.log(countries2);
+
+
+Central Africa
+ Angola	+244
+ Cameroon	+237
+ Central African Republic	+236
+ Chad	+235
+ Democratic Republic of the Congo	+243
+ Republic of the Congo	+242
+ Equatorial Guinea	+240
+ Gabon	+241
+ São Tomé and Príncipe	+239
+East Africa
+ Burundi	+257
+ Comoros	+269
+ Kenya	+254	
+ Madagascar	+261
+ Malawi	+265
+ Mauritius	+230
+ Mayotte (France)	+262
+ Mozambique	+258
+ Réunion (France)	+262
+ Rwanda	+250
+ Seychelles	+248
+ Tanzania	+255	
+ Uganda	+256	
+Horn of Africa
+ Djibouti	+253
+ Eritrea	+291	011 &
+ Ethiopia	+251
+ Somalia	+252
+North Africa
+ Algeria	+213
+ Egypt	+20
+ Libya	+218
+ Morocco	+212
+ South Sudan	+211
+ Sudan	+249
+ Tunisia	+216
+Southern Africa
+ Botswana	+267
+ Lesotho	+266
+ Namibia	+264
+ South Africa	+27
+ Swaziland	+268
+ Zambia	+260
+ Zimbabwe	+263
+West Africa
+ Ascension Island (United Kingdom)	+247
+ Benin	+229
+ Burkina Faso	+226
+ Cape Verde	+238
+ Ivory Coast	+225
+ Gambia	+220
+ Ghana	+233
+ Guinea	+224
+ Guinea-Bissau	+245
+ Liberia	+231
+ Mali	+223
+ Mauritania	+222
+ Niger	+227
+ Nigeria	+234
+ Saint Helena (United Kingdom)	+290
+ Senegal	+221
+ Sierra Leone	+232
+ Togo	+228
+ Tristan da Cunha (United Kingdom)	+290
